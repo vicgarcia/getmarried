399b0029a3a6e844fe9f2c50c71379aa791076fd
Vic Garcia <vic.garcia@outlook.com>
2017-05-01 02:00:39 -0500

    create database models

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from django.db.models import (
+        Model, DateTimeField, CharField, IntegerField, TextField, EmailField
+    )
+from django.contrib.postgres.fields import JSONField
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class RSVP(Model):
+    timestamp = DateTimeField(auto_now_add=True)
+    name = CharField(max_length=250)
+    guests = IntegerField()
+    note = TextField(null=False, blank=True)
+    phone = PhoneNumberField()
+
+
+class Gift(Model):
+    timestamp = DateTimeField(auto_now_add=True)
+    name = CharField(max_length=250)
+    email = EmailField()
+    order = CharField(max_length=250)   # stripe order id
+    amount = IntegerField()             # stripe returns amounts as cents
+    raw = JSONField()                   # save the entire stripe response
+
